2ea891cf15e8a2243739ad974cf5062dd4f68205
DUONNE MARLON PEREIRA DE ARAUJO <a9221872@rn.senac.br>
2018-10-09 20:38:59 -0300

    ok

--- src/main/java/br/senac/rn/loja/service/ProdutoService.java
+++ src/main/java/br/senac/rn/loja/service/ProdutoService.java
@@ -20,5 +20,10 @@ public class ProdutoService {
 	public List<Produto> obterTodos() {
 		return repository.findAll();
 	}
+
+	public void salvar(Produto produto) {
+		repository.save(produto);
+		
+	}
 	
 }
