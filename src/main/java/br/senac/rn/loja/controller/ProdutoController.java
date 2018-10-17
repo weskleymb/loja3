6fe28f43cc419dd9eae18e4819655cc3e87663d0
IRANILSON SOARES DA CRUZ <a93559@rn.senac.br>
2018-10-17 20:54:07 -0300

    17.10.18

--- src/main/java/br/senac/rn/loja/controller/ProdutoController.java
+++ src/main/java/br/senac/rn/loja/controller/ProdutoController.java
@@ -1,54 +1,33 @@
 package br.senac.rn.loja.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.senac.rn.loja.model.Departamento;
-import br.senac.rn.loja.model.Marca;
 import br.senac.rn.loja.model.Produto;
 import br.senac.rn.loja.service.DepartamentoService;
 import br.senac.rn.loja.service.MarcaService;
-import br.senac.rn.loja.service.ProdutoService;
 
 @Controller
 @RequestMapping("produto")
-public class ProdutoController {
+public class ProdutoController extends GenericController<Produto> {
 
-	@Autowired
-	private ProdutoService service;
 	@Autowired
 	private MarcaService marcaService;
 	@Autowired
 	private DepartamentoService departamentoService;
 	
-	@GetMapping
-	public String lista(Model model) {
-		List<Produto> produtos = service.obterTodos();
-		model.addAttribute("produtos", produtos);
-		return "produto/lista";
+	@Override
+	public Class<Produto> getClassType() {
+		return Produto.class;
 	}
 	
-	@GetMapping("cadastrar")
+	@Override
 	public String cadastrar(Model model) {
-		Produto produto = new Produto();
-		model.addAttribute("produto", produto);
-		List<Marca> marcas = marcaService.obterTodos();
-		model.addAttribute("marcas", marcas);
-		List<Departamento> departamentos = departamentoService.buscarTodos();
-		model.addAttribute("departamentos", departamentos);
-		return "produto/form";
-	}
-	
-	@PostMapping
-	public String salvar(Produto produto) {
-		service.salvar(produto);
-		return "redirect:/produto";
+		model.addAttribute("marcas", marcaService.obterTodos());
+		model.addAttribute("departamentos", departamentoService.obterTodos());
+		return super.cadastrar(model);
 	}
 	
 }
