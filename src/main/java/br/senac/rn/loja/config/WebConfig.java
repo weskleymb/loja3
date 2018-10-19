9acd27cf94485285974f67c642867d0a4eb452c3
Weskley Bezerra <weskleymb@hotmail.com>
2018-10-18 21:15:20 -0300

    adicao de autorizacao

---
+++ src/main/java/br/senac/rn/loja/config/WebConfig.java
@@ -0,0 +1,14 @@
+package br.senac.rn.loja.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+//	public void addViewController(ViewControllerRegistry registry) {
+//		registry.addViewController("/login").setViewName("login");
+//	}
+	
+}
---
+++ src/main/java/br/senac/rn/loja/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package br.senac.rn.loja.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.senac.rn.loja.model.Usuario;
+import br.senac.rn.loja.repository.UsuarioRepository;
+import br.senac.rn.loja.service.UsuarioDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UsuarioDetailsService user;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+		.antMatchers("/login", "/css/**", "/js/**", "/fonts/**").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()
+			.loginPage("/login").successForwardUrl("/marca")
+			.defaultSuccessUrl("/marca", true).permitAll();
+		super.configure(http);
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(user).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+}
