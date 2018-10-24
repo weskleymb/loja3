023c33b26a10af869a7b437fadfa864534e5df99
Weskley Bezerra <weskleymb@hotmail.com>
2018-10-24 00:04:37 -0300

    adicoes

--- src/main/java/br/senac/rn/loja/config/WebSecurityConfig.java
+++ src/main/java/br/senac/rn/loja/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,20 +22,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-		.antMatchers("/login", "/css/**", "/js/**", "/fonts/**", "/api/**").permitAll()
-		.antMatchers("/produto/**").hasRole("GERENTE")
-		.antMatchers("/departamento/**").hasRole("ADMIN")
+		.antMatchers("/login", "/css/**", "/js/**", "/fonts/**").permitAll()
 		.anyRequest().authenticated()
 		.and()
 		.formLogin()
 			.loginPage("/login").successForwardUrl("/marca")
 			.defaultSuccessUrl("/marca", true).permitAll();
-		super.configure(http);
 	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(user).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/api/**");
 	}
 	
 }
--- src/main/java/br/senac/rn/loja/controller/rest/DepartamentoRestController.java
+++ src/main/java/br/senac/rn/loja/controller/rest/DepartamentoRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,7 +31,7 @@ public class DepartamentoRestController {
 	}
 	
 	@PostMapping("/salvar")
-	public void salvar(Departamento departamento) {
+	public void salvar(@RequestBody Departamento departamento) {
 		repository.save(departamento);
 	}
 	
