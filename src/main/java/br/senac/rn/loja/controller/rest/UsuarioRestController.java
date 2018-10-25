88b9660be0d117ae5183ee9d9a5f532e7437ef38
Weskley Bezerra <weskleymb@hotmail.com>
2018-10-25 13:30:23 -0300

    heroku

--- src/main/java/br/senac/rn/loja/controller/rest/UsuarioRestController.java
+++ src/main/java/br/senac/rn/loja/controller/rest/UsuarioRestController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.senac.rn.loja.model.Usuario;
@@ -20,7 +19,7 @@ public class UsuarioRestController {
 	private UsuarioRepository usuarioRepository;
 	
 	@PostMapping("/salvar")
-	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
+	public ResponseEntity<?> salvar(@RequestBody Usuario usuario) {
 		Usuario u = usuarioRepository.save(usuario);
 		return ResponseEntity.status(HttpStatus.CREATED).body(u);
 	}
